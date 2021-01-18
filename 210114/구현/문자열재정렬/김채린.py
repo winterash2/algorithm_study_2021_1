6b8947691006e52505bb4fd495d13a3c6b8bd83f
zzerii <61822411+zzerii@users.noreply.github.com>
2021-01-18 10:38:14 +0900

    채린 문자열 재정렬 수정

--- 210114/구현/문자열재정렬/김채린.py
+++ 210114/구현/문자열재정렬/김채린.py
@@ -5,12 +5,16 @@ print(sort_arr)
 # ['1', '5', '7', 'A', 'B', 'C', 'K', 'K']
 
 arr_len=len(sort_arr)-1
-
+x=0
 for i in range(0,arr_len):
     if sort_arr[0].isdigit():
         #리스트 앞에꺼 팝해서 뒤에 붙여버리기~
-        x=sort_arr.pop(0)
-        sort_arr.append(x)
+        # x=sort_arr.pop(0)
+        # sort_arr.append(x)
+        x+=int(sort_arr[0])
+        sort_arr.pop(0)
+
+sort_arr.append(str(x))
 
 
 print(''.join(sort_arr))
