a92222e6bc71883e0dba5c5360b108e734c1250b
jung yura <jungyr24@hanmail.net>
2021-01-21 01:26:53 +0900

    경쟁적 전염 유라

---
+++ 210119/DFS/BFS/연구소/정유라.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+n, k = map(int, input().split())    
+graph = []
+data = []
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    for j in range(n):
+        if graph[i][j] != 
