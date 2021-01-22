96579122b5a5239cbe455b87916e79800c4e80b3
jung yura <jungyr24@hanmail.net>
2021-01-22 09:52:17 +0900

    미로탈출 유라 설명추가

--- 210118/DFS/미로탈출/정유라.py
+++ 210118/DFS/미로탈출/정유라.py
@@ -8,7 +8,7 @@ for i in range(n):
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
+# bfs이용
 def bfs(x, y):
     queue = deque()
     queue.append((x,y))
@@ -20,15 +20,20 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
+            # 범위 밖
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-
+            
+            # 벽
             if graph[nx][ny] == 0:
                 continue
-
+            
+            # 방문하지 않은 위치
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y]+1
-                queue.append((nx, ny))
+                queue.append((nx, ny)) # 큐에 삽입
+    
+    # 가장 오른쪽 아래까지의 최단 거리 반환
     return graph[n-1][m-1]
 
 print(bfs(0,0))
