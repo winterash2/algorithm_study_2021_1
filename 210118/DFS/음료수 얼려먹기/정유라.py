a446f650386700ea74d7fb75a9ac32d3b779fc3c
jung yura <jungyr24@hanmail.net>
2021-01-22 09:50:29 +0900

    음료수 얼려먹기 유라 설명추가

--- 210118/DFS/음료수 얼려먹기/정유라.py
+++ 210118/DFS/음료수 얼려먹기/정유라.py
@@ -3,12 +3,14 @@ arr = []
 
 for i in range(n):
     arr.append(list(map(int, input().split())))
-
+# dfs이용하여 특정 노드에 연결된 모든 노드 방문
 def dfs(x, y):
     if x >= n or x <= -1 or y <= 1 or y >= n:
         return False
     if arr[i][j] == 0:
         arr[i][j] = 1
+
+        # 상,하,좌,우 재귀적으로 호출 
         dfs(i-1, j)
         dfs(i, j-1)
         dfs(i+1, j)
@@ -17,6 +19,7 @@ def dfs(x, y):
         return True
     return False
 
+# 모든 위치에 댛여 음료수 채우기
 result = 0
 for i in range(n):
     for j in range(m):
