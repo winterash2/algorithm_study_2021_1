2858512451f05d6fcb48887118bad65a8d983a5f
baejinsoo <jinsoo941010@naver.com>
2021-01-25 19:38:40 +0900

    풀이방법 참고

--- 210126/이진탐색/정렬된배열에서특정수의개수구하기/배진수.py
+++ 210126/이진탐색/정렬된배열에서특정수의개수구하기/배진수.py
@@ -27,3 +27,23 @@ else:
     while sorting[right] == x:
         right += 1
     print(right - left - 1)
+
+#####################################################################
+# 파이썬 만세
+
+from bisect import bisect_left, bisect_right
+
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
+
+def count_by(array, left, right):
+    right = bisect_right(array, right)
+    left = bisect_left(array, left)
+    return right - left
+
+count = count_by(array, x, x)
+
+if count == 0:
+    print(-1)
+else:
+    print(count)
