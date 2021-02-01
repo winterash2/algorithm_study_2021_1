441ccd6ac6853ea5db8e29280ee2bd1af060ae71
winterash2 <winterash2@naver.com>
2021-02-01 23:25:15 +0900

    2021-02-01-이동재-편집 거리

---
+++ 210129/다이나믹 프로그래밍/편집 거리/이동재.py
@@ -0,0 +1,29 @@
+# str1 = list(input())
+# str2 = list(input())
+
+INF = float("INF")
+
+base = list("sunday")
+goal = list("saturday")
+base = list("cut")
+goal = list("cat")
+
+
+dp = [[INF for _ in range(len(goal))] for _ in range(len(base))]
+for j in range(len(goal)):
+    dp[0][j] = j
+for i in range(len(base)):
+    dp[i][0] = i
+
+for i in range(1, len(base)):
+    for j in range(1, len(goal)):
+        if base[i] == goal[j]:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = 1 + min(
+                    dp[i-1][j-1],
+                    dp[i-1][j],
+                    dp[i][j-1]
+                )
+
+print(dp[len(base)-1][len(goal)-1])
