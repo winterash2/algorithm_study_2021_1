c1f086b8527be2e51b9982f8f70dab884cbd7ed9
baejinsoo <jinsoo941010@naver.com>
2021-02-03 16:03:11 +0900

    스터디 후 오답문제 수정

--- 210129/다이나믹 프로그래밍/못생긴 수/배진수.py
+++ 210129/다이나믹 프로그래밍/못생긴 수/배진수.py
@@ -1,19 +1,25 @@
-list_2 = [2]
-list_3 = [3]
-list_5 = [5]
+ugly = [0] * n # 못생긴 수를 담기 위한 테이블 (1차원 DP 테이블)
+ugly[0] = 1 # 첫 번째 못생긴 수는 1
 
-for i in range(900):
-    list_2.append(2*(i+2))
-    list_3.append(3*(i+2))
-    list_5.append(5*(i+2))
+# 2배, 3배, 5배를 위한 인덱스
+i2 = i3 = i5 = 0
+# 처음에 곱셈 값을 초기화
+next2, next3, next5 = 2, 3, 5
 
-res = [1]
-res = res + list_2 + list_3 + list_5
-res.sort()
-result = set(res)
-a = list(result)
+# 1부터 n까지의 못생긴 수들을 찾기
+for l in range(1, n):
+    # 가능한 곱셈 결과 중에서 가장 작은 수를 선택
+    ugly[l] = min(next2, next3, next5)
+    # 인덱스에 따라서 곱셈 결과를 증가
+    if ugly[l] == next2:
+        i2 += 1
+        next2 = ugly[i2] * 2
+    if ugly[l] == next3:
+        i3 += 1
+        next3 = ugly[i3] * 3
+    if ugly[l] == next5:
+        i5 += 1
+        next5 = ugly[i5] * 5
 
-n = int(input())
-# print(a[n-1])
-for i in range(n):
-    print(list_2[i] ,end=" ")
+# n번째 못생긴 수를 출력
+print(ugly[n - 1])
--- 210129/다이나믹 프로그래밍/편집 거리/배진수.py
+++ 210129/다이나믹 프로그래밍/편집 거리/배진수.py
@@ -1,55 +1,26 @@
+INF = int(1e9)
+
 A = input()
 B = input()
 
-list_a = []
-list_b = []
+dp = [[INF for _ in range(len(B)+1)] for _ in range(len(A)+1)]
+dp[0][0] = 0
 
-for a in A:
-    list_a.append(a)
-for b in B:
-    list_b.append(b)
+for i in range(len(B)):
+    dp[0][i+1] = ord(B[i])
 
-count = 0
+for i in range(len(A)):
+    dp[i+1][0] = ord(A[i])
 
-# 길이가 다를 때
-if len(list_a) != len(list_b):
-    diff = abs(len(list_a) - len(list_b))
-    # 다른 길이 만큼 짧은 쪽에 0을 넣어주고 두 문자열이 같아질때까지 비교 후 총 결과 값에 diff * 2 만큼 빼줌(0을 넣기때문에 다르고, 원래는 그 위치에 맞게 들어가기 때문)
-    if len(list_a) < len(list_b):
-        for _ in range(diff):
-            list_a.append(0)
-        for i in range(len(list_b)):
-            if list_a[i] != list_b[i]:
-                count += 1
-                if i != len(list_b):
-                    temp = list_a[i+1:]
-                    list_a[i] = list_b[i]
-                    list_a = list_a[:i+1] + temp
-                else:
-                    list_a.append(list_b[i])
-    else:
-        for _ in range(diff):
-            list_b.append(0)
-        for i in range(len(list_a)):
-            if list_a[i] != list_b[i]:
-                count += 1
-                if i != len(list_a):
-                    temp = list_a[i+1:]
-                    list_a = list_a[:i] + temp
-                else:
-                    list_a = list_a[:i]
-    count -= (diff * 2)
-# 길이가 같을 때
-else:
-    for i in range(len(list_b)):
-        if list_a[i] != list_b[i]:
-            count += 1
-            if i != len(list_b):
-                temp = list_a[i+1:]
-                list_a[i] = list_b[i]
-                list_a = list_a[:i+1] + temp
-            else:
-                list_a.append(list_b[i])
+for i in range(1, len(A) + 1):
+    for j in range(1, len(B) + 1):
+        if dp[0][j] == dp[i][0]:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
 
-# print(list_a, list_b)
-print(count)
+print(dp[-1][-1])
+for i in range(len(A)+1):
+    for j in range(1, len(B) + 1):
+        print(dp[i][j], end= "")
+    print()
