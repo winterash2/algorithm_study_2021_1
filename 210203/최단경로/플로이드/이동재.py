46a1ef891c8682f2eca2587ef2edc42565558dd9
winterash2 <winterash2@naver.com>
2021-02-04 10:40:46 +0900

    2021-02-04-이동재-플로이드-배열출력 편하게 하는 법

--- 210203/최단경로/플로이드/이동재.py
+++ 210203/최단경로/플로이드/이동재.py
@@ -22,3 +22,25 @@ for i in range(1, N+1):
     for j in range(1, N+1):
         print(floyd[i][j] if floyd[i][j] != INF else 0, end=" ")
     print()
+
+for i in range(1, N+1):
+    print(*floyd[i][1:])
+
+"""
+5
+14 
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+3 5 10
+3 1 8
+1 4 2
+5 1 7
+3 4 2
+5 2 4
+"""
