c6dface247600d93343ca1631c111d2b4ce194c7
baejinsoo <jinsoo941010@naver.com>
2021-02-18 23:42:59 +0900

    solve 문제집 진수

--- 210218-210219/백준_1766/배진수.py
+++ 210218-210219/백준_1766/배진수.py
@@ -1,3 +1,4 @@
+"""
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -36,3 +37,39 @@ def topology():
 
 result = topology()
 
+"""
+import heapq
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+indegree = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+
+def topology():
+    result = []
+    q = []
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            heapq.heappush(q, i)
+
+    while q:
+        now = heapq.heappop(q)
+        result.append(now)
+        graph[now].sort()
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                heapq.heappush(q, i)
+
+    return result
+
+result = topology()
+for res in result:
+    print(res, end=" ")
