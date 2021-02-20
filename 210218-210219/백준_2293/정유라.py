cb5b5fceedcfde75fdb89cf1550c19de3a8913f3
jung yura <jungyr24@hanmail.net>
2021-02-20 11:59:53 +0900

    동전1 유라

---
+++ 210218-210219/백준_2293/정유라.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+dp = [0] * (k+1)
+dp[0] = 1
+coins = []
+for _ in range(n):
+    coin = int(input())
+    coins.append(coin)
+
+for coin in coins:
+    for j in range(coin, k+1):
+        dp[j] += dp[j-coin]
+print(dp)
+print(dp[k])
