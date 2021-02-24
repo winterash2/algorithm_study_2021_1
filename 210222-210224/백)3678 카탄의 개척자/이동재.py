789de2a72469c2b1b00d909adf4e298a13a465bc
winterash2 <winterash2@naver.com>
2021-02-25 02:56:58 +0900

    2021-02-25-이동재-카탄의 개척자-성공!

--- 210222-210224/백)3678 카탄의 개척자/이동재.py
+++ 210222-210224/백)3678 카탄의 개척자/이동재.py
@@ -1,49 +1,52 @@
 # 배열 사용하는 방식으로도 해봤는데 이것도 실패함...
 # 어떻게 하는지는 아는데... 구현 문제 진짜... 짜증난다...
+# 오 성공함. 문제 진짜 엿같네
 import sys
-sys.stdout = open('output.txt','w')
+# sys.stdout = open('output.txt','w')
 
 C = int(input())
 
 for _ in range(C):
     N = int(input())
-    graph = [[0 for _ in range(400)] for _ in range(200)]
+    graph = [[0 for _ in range(140)] for _ in range(280)]
     # 방향은 우상향 -> 상향 -> 좌상향 -> 좌하향 -> 하향 -> 우하향 ---- 이걸 2번 반복한 배열을 만들어놓음
-    directions = [(1, -1), (0, -2), (-1, -1), (-1, 1), (0, 2), (1, 1), (1, -1), (0, -2), (-1, -1), (-1, 1), (0, 2), (1, 1)]
+    directions = [(-1, 1), (-2, 0), (-1, -1), (1, -1), (2, 0), (1, 1), (-1, 1), (-2, 0), (-1, -1), (1, -1), (-2, 0), (1, 1)]
     idxD = 0
-    graph[100][201] = 6
-    graph[100][200] = 1
-    graph[101][199] = 2
-    graph[100][198] = 3
-    graph[99][199] = 4
-    graph[99][201] = 5
-    graph[100][202] = 2
-    graph[101][201] = 3
-    x = 101
-    y = 201
-    useCount = [1e9, 0, 0, 0, 0, 0]
+    graph[140][70] = 1
+    graph[139][71] = 2
+    graph[138][70] = 3
+    graph[139][69] = 4
+    graph[141][69] = 5
+    graph[142][70] = 2
+    graph[141][71] = 3
+    x = 71
+    y = 141
+    useCount = [1e9, 1, 2, 2, 1, 1]
     result = [0, 1, 2, 3, 4, 5, 2, 3] + [0 for _ in range(N-7)]
     #7까지 초기화
     for i in range(8, N+1):
         # 방향을 바꿀 수 있는지 확인, 바꿀 수 있으면 다음 방향으로 바꿈
-        nx, ny = x + directions[idxD+1][0], y + directions[idxD+1][1]
-        if graph[nx][ny] == 0:
+        nx, ny = x + directions[idxD+1][1], y + directions[idxD+1][0]
+        if graph[ny][nx] == 0:
             idxD += 1
             if idxD >= 6:
                 idxD -= 6
         # 방향을 바꿨거나 안 바꿨거나 어쨋것 이번에 채울 칸 위치
-        x, y = x + directions[idxD][0], y + directions[idxD][1]
-        cant = [graph[x][y-2], graph[x-1][y-1], graph[x+1][y-1], graph[x-1][y+1], graph[x+1][y+1], graph[x][y+2]]
-        canTile = list(set([1,2,3,4,5]) - set([graph[x][y-2], graph[x-1][y-1], graph[x+1][y-1], graph[x-1][y+1], graph[x+1][y+1], graph[x][y+2]]))
+        x, y = x + directions[idxD][1], y + directions[idxD][0]
+        # print(x,y)
+        canTile = list(set([1,2,3,4,5]) - set([graph[y-2][x], graph[y-1][x-1], graph[y-1][x+1], graph[y+1][x-1], graph[y+1][x+1], graph[y+2][x]]))
         canTile.sort(reverse=True)
+        # 확인된 것들 중 useCount가 가장 낮으면서, 낮은게 여러개 일 때는 수가 적은 것 찾음
         minIdx = 0
         for c in canTile:
             if useCount[c] <= useCount[minIdx]:
                 minIdx = c
-        graph[x][y] = c
-        result[i] = c
+        useCount[minIdx] += 1
+        graph[y][x] = minIdx
+        result[i] = minIdx
     print(result[N])
-    [print(graph[i]) for i in range(200)]
+    # print(useCount)
+    # [print(graph[i]) for i in range(len(graph))]
 
 
 # 배열 안 쓰고 해보려고 했는데 실패함
