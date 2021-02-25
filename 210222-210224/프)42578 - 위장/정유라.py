b4963bec2edcdbdc98abbb0c6c59598b7f8c0720
jung yura <jungyr24@hanmail.net>
2021-02-25 16:23:36 +0900

    위장 유라 못 품

--- 210222-210224/프)42578 - 위장/정유라.py
+++ 210222-210224/프)42578 - 위장/정유라.py
@@ -1,1 +1,8 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42578
+clothes = [[yellow_hat, headgear], [blue_sunglasses, eyewear], [green_turban, headgear]]
+
+from itertools import combinations
+def solution(clothes):
+    
+    answer = 0
+    return total
