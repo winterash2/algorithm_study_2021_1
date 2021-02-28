848899e95aa36ca98c69d38ec842efb6f1860e67
jung yura <jungyr24@hanmail.net>
2021-02-28 18:11:29 +0900

    웜홀 유라

--- 210225-210228/백)1865 웜홀/정유라.py
+++ 210225-210228/백)1865 웜홀/정유라.py
@@ -11,41 +11,31 @@
 # * 만약 음수간선 사이클 발생하는지 체크-> 3번 과정을 한번 더 수행, 이때 최단거리 테이블이 갱신된다면 음수간선사이클 존재.
 
 
-def bfs(start):
-    q = []
-    distance[start] = 0
-    heapq.heappush(q, (0, start))
-    total = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-
-        for i in graph[now]:
-            cost = distance[now] + 1
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                total += i[1]
-                heapq.heappush(q, (cost, i[0]))     
+def bf(start):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            for now, cost in graph[j]:
+                
+                if distance[now] > distance[j] + cost:
+                    distance[now] = distance[j] + cost
+                    if i == n:
+                        return True
+    return False
     
 for _ in range(int(input())):
     n, m, w = map(int, input().split())
     graph = [[] for _ in range(n+1)]
+    distance = [1e9] * (n+1)
+
     for i in range(m):
         s, e, t = map(int, input().split())
-        # graph[s][e] = t
-        # graph[e][s] = t
         graph[s].append((e, t))
         graph[e].append((s, t))
     for _ in range(w):
         s, e, t = map(int, input().split())
-        # graph[s][e] = -t
         graph[s].append((e, -t))
-    possible = False 
-    for i in range(1, n+1):
-        distance = [1e9] * (n+1)
-        bfs(i)
-        print(distance)
+
+    possible = bf(i)
 
     if possible:
         print("YES")
