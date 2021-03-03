9cee5df65031d8e7dc91d2599544b2923bab68ee
jung yura <jungyr24@hanmail.net>
2021-03-03 22:21:45 +0900

    공항 유라

---
+++ 210301-210303/백)10775 공항/정유라.py
@@ -0,0 +1,34 @@
+# http://boj.kr/10775
+
+g = int(input())
+p = int(input())
+
+parnet = [0] * (g+1)
+for i in range(1, g+1):
+    parnet[i] = i
+
+def find(parent, x):
+    if parnet[x] != x:
+        parnet[x] = find(parnet, parnet[x])
+    return parnet[x]
+
+def union(parent, a, b):
+    a = find(parnet, a)
+    b = find(parnet, b)
+
+    if a < b:
+        parnet[b] = a
+    else:
+        parnet[a] = b
+
+
+result = 0
+for i in range(p):
+    plane = int(input())
+    plane_root = find(parnet, plane)
+    if plane_root == 0:
+        break
+    union(parnet, plane_root, plane_root-1)
+    result += 1
+
+print(result)
