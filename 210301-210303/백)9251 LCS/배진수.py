41afd70b426d0f548d87c726404ce0bad58cd2cc
baejinsoo <jinsoo941010@naver.com>
2021-03-03 18:37:19 +0900

    solve LCS 진수

---
+++ 210301-210303/백)9251 LCS/배진수.py
@@ -0,0 +1,15 @@
+X = input()
+Y = input()
+
+n = len(X)
+m = len(Y)
+
+lcs = [[0] * (m+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if X[i-1] == Y[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i][j-1], lcs[i-1][j])
+
+print(lcs[n][m])
