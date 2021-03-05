656d1c171f4fbf28908dd15a5e0c96b04657e25e
winterash2 <winterash2@naver.com>
2021-03-05 23:55:17 +0900

    2021-03-05-이동재-후위 표기식-2차

--- 210304-210305/백)1918 후위 표기식/이동재.py
+++ 210304-210305/백)1918 후위 표기식/이동재.py
@@ -1,3 +1,40 @@
+# 2차 시도, 틀렸다고 나오는데 왜 틀린지 모르겠음
+def conversion(string):
+    if len(string) == 1:
+        return string
+    pre = []
+    suf = []
+    oper = ''
+    if string[0] != '(':
+        pre = string[0]
+        oper = string[1]
+        suf = string[2:]
+    else:
+        depth = 0
+        for i in range(len(string)):
+            if string[i] == '(':
+                depth += 1
+            elif string[i] == ')':
+                depth -= 1
+                if depth == 0:
+                    pre = string[1:i]
+                    if i != len(string) - 1:
+                        oper = string[i+1]
+                        suf = ''.join(string[i+2:])
+                    break
+
+    if oper == '':
+        return conversion(pre)
+    else:
+        return conversion(pre) + conversion(suf) + oper
+
+
+string = input()
+print(conversion(string))
+
+
+# 1차 시도, 틀렸다고 나오는데 왜 틀린지 모르겠음
+"""
 orig = list(input())
 
 
@@ -39,3 +76,4 @@ def conversion(string):
 
 
 print(conversion(orig))
+"""
