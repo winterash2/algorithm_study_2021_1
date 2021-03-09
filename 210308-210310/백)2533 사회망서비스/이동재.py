64de281599d3dce2bd419329e06392f109f1b327
winterash2 <winterash2@naver.com>
2021-03-09 21:14:17 +0900

    2021-03-09-이동재-사회망 서비스-5차-성공

--- 210308-210310/백)2533 사회망서비스/이동재.py
+++ 210308-210310/백)2533 사회망서비스/이동재.py
@@ -1,20 +1,51 @@
-"""
-14
-1 2
-1 3
-1 4
-2 5
-2 6
-4 7
-4 8
-7 9
-9 10
-9 11
-9 12
-9 13
-9 14
-"""
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+tree = [[] for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
+for _ in range(N-1):
+    u, v = map(int, input().split())
+    tree[u].append(v)
+    tree[v].append(u) # 입력을 이쁘게 안 줄수도 있어서 양방향으로 넣어야 함
+
+# dp[n][0] 은 자기가 포함 안 된 경우 dp[n][1] 은 자기가 포함 된 경우
+dp = [[0, 0] for _ in range(N+1)]
+
+q = [1]
+for elem in q:
+    visited[elem] = True
+    for f in tree[elem]:
+        if visited[f]:
+            continue
+        q.append(f)
+
+q = q[::-1]
+for id in q:
+    dp[id][0] = 0
+    dp[id][1] = 1
+    for f in tree[id]:
+        dp[id][0] += dp[f][1]
+        dp[id][1] += min(dp[f][0], dp[f][1])
+
+print(min(dp[1][0], dp[1][1]))
+
+# def dfs(id):
+#     dp[id][0] = 0
+#     dp[id][1] = 1
+#     for f in tree[id]:
+#         if visited[id]: # 양방향으로 넣었기 때문에 역으로는 가면 안 되기 때문
+#             continue
+#         dfs(f)
+#         dp[id][0] += dp[f][1]
+#         dp[id][1] += min(dp[f][0], dp[f][1])
+
+# dfs(1)
+# print(min(dp[1][0], dp[1][1]))
+
+
 # 이건 맞긴 한데 recursion error 뜸
+"""
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(1000001)
@@ -42,7 +73,7 @@ def dfs(id):
 
 dfs(1)
 print(min(dp[1][0], dp[1][1]))
-
+"""
 
 # 자식 노드에서 부모 노드로 타고 내려가면서 계산 - 실패
 """
@@ -157,4 +188,21 @@ for _ in range(N-1):
 answer = [0, 0]
 solution(node, 1, 0, answer)
 print(min(answer))
+"""
+
+"""
+14
+1 2
+1 3
+1 4
+2 5
+2 6
+4 7
+4 8
+7 9
+9 10
+9 11
+9 12
+9 13
+9 14
 """
