de32e1512d80cb32a0907f24f4a423f7e7bda2d3
jung yura <jungyr24@hanmail.net>
2021-03-10 18:14:12 +0900

    줄세우기 유라

--- 210304-210305/백) 2252 줄세우기/정유라.py
+++ 210304-210305/백) 2252 줄세우기/정유라.py
@@ -1,1 +1,31 @@
 # http://boj.kr/2252
+from collections import deque
+
+n, m = map(int, input().split())
+indegree = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+    indegree[b] += 1
+
+q = deque()
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+result = []
+# 위상정렬
+while q:
+    now = q.popleft()
+    result.append(now)
+
+    for i in graph[now]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            q.append(i)
+
+for i in result:
+    print(i,end=" ")
