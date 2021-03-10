08c5b6640cb55001a55c5126115203a0b11ceead
jung yura <jungyr24@hanmail.net>
2021-03-10 18:31:56 +0900

    조합 유라 factorial 구현

--- 210304-210305/백) 2407 조합/정유라.py
+++ 210304-210305/백) 2407 조합/정유라.py
@@ -6,3 +6,14 @@ n, m = map(int, input().split())
 up = math.factorial(n)
 down = (math.factorial(n-m)) * (math.factorial(m))
 print(up // down)
+
+# def fac(i):
+#     result = 1
+#     while i > 1:
+#         result *= i
+#         i -= 1
+#     return result
+
+# up = fac(n)
+# down = (fac(n-m)) * (fac(m))
+# print(up // down)
