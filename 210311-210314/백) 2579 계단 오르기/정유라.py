454f569a7a0bac3ebdd541ab2d062bec132fde00
jung yura <jungyr24@hanmail.net>
2021-03-12 14:25:56 +0900

    계단

--- 210311-210314/백) 2579 계단 오르기/정유라.py
+++ 210311-210314/백) 2579 계단 오르기/정유라.py
@@ -1,14 +1,17 @@
 # http://boj.kr/2579
 
 n = int(input())
-stairs = []
+stairs = [(0, 0)]
 for _ in range(n):
-    stairs.append(int(input()))
-
+    stairs.append((int(input()), 0))
+print(stairs)
 # 1. 1칸 움직임 -> 2칸 움직여야함
 # 2. 2칸 움직임 -> 1칸 or 2칸 가능
 
 # dp배열 안에 (value, 몇칸움직여왔는지 저장)
 # 1이면 dp[i+2] = dp[i] + stairs[i+2]
 # dp[i]의 값이(value, 2)면 1칸: dp[i+1] = max(dp[i+1], dp[i]+stairs[i+1])
-#                         2칸: dp[i+2] = max(dp[i+2], dp[])               
+#                         2칸: dp[i+2] = max(dp[i+2], dp[])
+
+# for i in range(n):
+    
