aa25e9d805ab3c232d92c027495d6e2db8d47bd8
winterash2 <winterash2@naver.com>
2021-03-12 14:00:07 +0900

    2021-03-12-이동재-이진 검색 트리-2차-실패

--- 210311-210314/백) 5639 이진 검색 트리/이동재.py
+++ 210311-210314/백) 5639 이진 검색 트리/이동재.py
@@ -1,8 +1,9 @@
+# 2차 deque로 푼 것
+# 이것도 시간 초과 뜸
+"""
 from collections import deque
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)
-
 
 arr = []
 while True:
@@ -11,11 +12,14 @@ while True:
     except:
         break
 
-
-def loop(arr):
+answer = []
+q = deque()
+q.append(arr)
+while q:
+    arr = q.popleft()
     if len(arr) <= 1:
-        return arr
-    # 리스트일 때
+        answer = answer + arr
+        continue
     root = arr[0]
     left = []
     right = []
@@ -24,14 +28,12 @@ def loop(arr):
             left.append(elem)
         else:
             right.append(elem)
+    q.appendleft([root])
+    q.appendleft(right)
+    q.appendleft(left)
 
-    left = loop(left)
-    right = loop(right)
-    return left + right + [root]
-
-
-answer = loop(arr)
 [print(x) for x in answer]
+"""
 
 # 1차 recursion으로 푼 것
 # 시간 초과 됨
