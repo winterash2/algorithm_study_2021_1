bdcf789a7f7731b23b56bd775ef59d43b2f03d0d
winterash2 <winterash2@naver.com>
2021-03-12 19:38:10 +0900

    2021-03-12-이동재-벽 부수고 이동하기-2차-실패

--- 210311-210314/백)2206 벽 부수고 이동하기/이동재.py
+++ 210311-210314/백)2206 벽 부수고 이동하기/이동재.py
@@ -1,3 +1,65 @@
+from collections import deque
+import heapq
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = []
+for _ in range(N):
+    graph.append([int(x) for x in input().split()[0]])
+
+
+dp = [[1e9 for _ in range(M)] for _ in range(N)]
+q = deque()
+q.append((0, 0))
+depth = 0
+qDict = dict()
+while q:
+    depth += 1
+    for _ in range(len(q)):
+        x, y = q.popleft()
+        if x < 0 or x >= N or y < 0 or y >= M or dp[x][y] != 1e9:
+            continue
+        if graph[x][y] == 1:
+            for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx = x + d[0]
+                ny = y + d[1]
+                if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 0:
+                    if (nx, ny) not in qDict:
+                        qDict[(nx, ny)] = depth + 1
+            continue
+        dp[x][y] = depth
+        for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx = x + d[0]
+            ny = y + d[1]
+            if 0 <= nx < N and 0 <= ny < N:
+                q.append((nx, ny))
+answer = dp[N-1][M-1]
+
+q = []
+for key in qDict.keys():
+    x, y = key
+    q.append((qDict[key], x, y))
+
+dp = [[1e9 for _ in range(M)] for _ in range(N)]
+heapq.heapify(q)
+while q:
+    depth, x, y = heapq.heappop(q)
+    dp[x][y] = depth
+    for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nx = x + d[0]
+        ny = y + d[1]
+        if 0 <= nx < N and 0 <= ny < M and dp[nx][ny] == 1e9 and graph[nx][ny] == 0:
+            heapq.heappush(q, (depth+1, nx, ny))
+
+answer = min(answer, dp[N-1][M-1])
+if answer == 1e9:
+    print(-1)
+else:
+    print(answer)
+
+
+"""
 # 1차 시도. 벽을 하나씩 없애고 전부 다시 구하는 방식
 # 시간초과됨
 from collections import deque
@@ -48,3 +110,4 @@ if answer == 1e9:
     print(-1)
 else:
     print(answer)
+"""
