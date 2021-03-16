d11018763f21509be3ee98fa0d73f6528d4bfb0c
winterash2 <winterash2@naver.com>
2021-03-17 00:31:31 +0900

    2021-03-17-이동재-열쇠

--- 210315-210318/백)9328 열쇠/이동재.py
+++ 210315-210318/백)9328 열쇠/이동재.py
@@ -2,6 +2,7 @@ T = int(input())
 for _ in range(T):
     H, W = map(int, input().split())
     graph = []
+    visited = [[False for _ in range(W)] for _ in range(H)]
     for _ in range(H):
         graph.append(list(input()))
     keys = [x.upper() for x in list(input()) if x != '0']
@@ -11,35 +12,56 @@ for _ in range(T):
     for y in range(H):
         for x in range(W):
             if y == 0 or x == 0 or y == H-1 or x == W-1:
-                if graph[y][x] == '.':
-                    q.append((x, y, []))
+                if graph[y][x] == '.' or graph[y][x] == '$':
+                    q.append((x, y))
+                elif graph[y][x] == '*':
+                    pass
+                else:
+                    if graph[y][x].isupper():
+                        q.append((x, y))
+                    else:
+                        keys.add(graph[y][x])
+                        q.append((x,y))
+                        graph[y][x] = '.'
 
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-    needKeyForKey = []
-    needKeyForDoc = []
+    answer = 0
+    blocked = []
+    count = 0
     while q:
-        x, y, need = q.pop()
-        if graph[y][x] == '*':  # 벽이면 스킵
-            continue
-        if graph[y][x] == '.':  # 길이면 이동
-            pass
-        elif graph[y][x] == '$':  # 문서면 이동과 획득
-            needKeyForDoc.append((x, y, need))
-        else: # 문이거나 키면 이동과 기록
-            if graph[y][x].isupper():  # 문
-                need.append(graph[y][x])
-            else:  # 키
-                needKeyForKey.append((graph[y][x], need))
+        changed = False
+        while q:
+            x, y = q.pop()
+            visited[y][x] = True
+            if graph[y][x] == '.':
+                pass  # 그냥 지나감
+            elif graph[y][x] == '$':
+                graph[y][x] = '.'
+                answer += 1
+            elif graph[y][x].islower():  # 키일때
+                keys.add(graph[y][x].upper())
+                graph[y][x] = '.'
+                changed = True
+            else:  # 문일때
+                if graph[y][x] not in keys:
+                    blocked.append((x, y))
+                    continue
+                else:
+                    graph[y][x] = '.'
+            for d in directions:
+                nx, ny = x + d[0], y + d[1]
+                if 0 <= nx < W and 0 <= ny < H:
+                    if graph[ny][nx] != '*':
+                        if not visited[ny][nx]:
+                            q.append((nx, ny))
+        if not changed:
+            break
+        q = q + blocked
+        blocked = []
 
-        for d in directions:
-            nx = x + d[0]
-            ny = y + d[1]
-            if 0 <= nx < W and 0 <= ny < H:
-                q.append(nx, ny, need)
-    print("needKeyForKey", needKeyForKey)
-    print("needKeyForDoc", needKeyForDoc)
-
+    # [print(''.join(x)) for x in graph]
+    print(answer)
 
 # 1차 시도중 문제가 있을거 같아서 방법을 바꿔보려고 함
 # 여기서 시도한 방법은 입구로부터 어딘가에 도달할 때 필요한 키들을 저장해놓고
