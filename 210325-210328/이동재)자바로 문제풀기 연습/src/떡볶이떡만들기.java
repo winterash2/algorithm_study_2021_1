ff294958b7ef484c08a2f0c92592256f9214c627
winterash2 <winterash2@naver.com>
2021-03-30 22:17:46 +0900

    2021-03-30-이동재-떡볶이 떡 만들기

---
+++ 210325-210328/이동재)자바로 문제풀기 연습/src/떡볶이떡만들기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class 떡볶이떡만들기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] dducks = new int[N];
+        for(int i=0; i<N; i++){
+            dducks[i] = sc.nextInt();
+        }
+        int left = 1;
+        int right = 100000000;
+        int answer = 0;
+        while(left < right){
+            int mid = (left + right) / 2;
+            int count = 0;
+            for(int i=0; i<N; i++){
+                int sub = dducks[i] - mid;
+                if(sub > 0){
+                    count += sub;
+                }
+            }
+            if(count >= M){
+                answer = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
+/*
+4 6
+19 15 10 17
+ */
